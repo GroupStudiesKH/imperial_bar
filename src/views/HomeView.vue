f73af98ef50b8e152faf17287c357369aa68e489
mafiapatch <g8071107@gmail.com>
2024-03-19 22:40:24 +0800

    [modify] carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -37,165 +37,347 @@ export default {
         <div class="col-12 px-5 areaHeading">
           <h5>北部店家</h5>
         </div>
-        <div class="barContainer">
-          <div class="barRow">
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
+        <div id="carouselNorth" class="carousel slide">
+          <div class="carousel-inner barContainer">
+            <div class="barRow carousel-item active">
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun1</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
                 </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
+              </div>
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun2</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
+            <div class="barRow carousel-item">
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
                 </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
+              </div>
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div class="barRow">
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
-                </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
-                </div>
-              </div>
-            </div>
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
-                </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
+            <div class="barRow carousel-item">
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-
-          <div class="barRow">
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
-                </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
-                </div>
-              </div>
-            </div>
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
-                </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="barRow">
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
-                </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
-                </div>
-              </div>
-            </div>
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
-                </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <div class="barRow">
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
-                </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
-                </div>
-              </div>
-            </div>
-            <div class="barCol">
-              <div class="octagonBar">
-                <div class="title">標題標題標題標題</div>
-                <div class="bar_img">
-                  <img src="/assets/img/bar_image_example.png" alt="1" />
-                </div>
-                <div class="bar_title">
-                  <h5>Pun</h5>
-                </div>
-                <div class="bar_address">
-                  <p>📍台北市大安區四段378巷5號1樓</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselNorth"
+            data-bs-slide="prev"
+          >
+            <img src="/assets/img/carousel_prev.png" />
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselNorth"
+            data-bs-slide="next"
+          >
+            <img src="/assets/img/carousel_next.png" />
+          </button>
         </div>
 
+        <div class="col-12 px-5 areaHeading">
+          <h5>中部店家</h5>
+        </div>
+        <div id="carouselMid" class="carousel slide">
+          <div class="carousel-inner barContainer">
+            <div class="barRow carousel-item active">
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun1</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun2</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="barRow carousel-item">
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="barRow carousel-item">
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselMid"
+            data-bs-slide="prev"
+          >
+            <img src="/assets/img/carousel_prev.png" />
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselMid"
+            data-bs-slide="next"
+          >
+            <img src="/assets/img/carousel_next.png" />
+          </button>
+        </div>
 
+        <div class="col-12 px-5 areaHeading">
+          <h5>南部店家</h5>
+        </div>
+        <div id="carouselSouth" class="carousel slide">
+          <div class="carousel-inner barContainer">
+            <div class="barRow carousel-item active">
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun1</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun2</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="barRow carousel-item">
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="barRow carousel-item">
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+              <div class="barCol">
+                <div class="octagonBar">
+                  <div class="title">標題標題標題標題</div>
+                  <div class="bar_img">
+                    <img src="/assets/img/bar_image_example.png" alt="1" />
+                  </div>
+                  <div class="bar_title">
+                    <h5>Pun</h5>
+                  </div>
+                  <div class="bar_address">
+                    <p>📍台北市大安區四段378巷5號1樓</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselSouth"
+            data-bs-slide="prev"
+          >
+            <img src="/assets/img/carousel_prev.png" />
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselSouth"
+            data-bs-slide="next"
+          >
+            <img src="/assets/img/carousel_next.png" />
+          </button>
+        </div>
       </div>
+
+      
       <div id="page_bottom">
         <img src="/assets/img/previous.png" />
       </div>
