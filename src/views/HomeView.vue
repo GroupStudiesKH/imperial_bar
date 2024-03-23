ba82f4b94fc6dca50199b0d3ab65f829fd1e7147
mafiapatch <g8071107@gmail.com>
2024-03-23 16:56:46 +0800

    [modify] update data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -53,7 +53,7 @@ export default {
       //scroll 290px
       const carousel = document.getElementById(carouselId);
       const scrollAmount = 275;
-      const scrollDuration = 275; // Adjust the duration as needed
+      const scrollDuration = 300; // Adjust the duration as needed
 
       switch (carouselId) {
         case `northShop`:
@@ -101,7 +101,7 @@ export default {
       //scroll 290px
       const carousel = document.getElementById(carouselId);
       const scrollAmount = 275;
-      const scrollDuration = 275; // Adjust the duration as needed
+      const scrollDuration = 300; // Adjust the duration as needed
       switch (carouselId) {
         case `northShop`:
           northPage.value--;
@@ -272,7 +272,7 @@ export default {
             <div class="barRow" id="southShop">
               <div
                 class="barCol"
-                v-for="(shop, shopIndex) in northShopData"
+                v-for="(shop, shopIndex) in southShopData"
                 :key="shopIndex"
               >
                 <router-link :to="'/detail/' + shop.id">
